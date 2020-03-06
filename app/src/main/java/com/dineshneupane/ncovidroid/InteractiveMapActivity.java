a595e43e04819e6b96bc79d3a677ca6a3580f3a9
Nithin <nithin2889@gmail.com>
2020-03-06 22:47:11 +0530

    cosmetic changes to interactive map activity

--- app/src/main/java/com/dineshneupane/ncovidroid/InteractiveMapActivity.java
+++ app/src/main/java/com/dineshneupane/ncovidroid/InteractiveMapActivity.java
@@ -1,16 +1,19 @@
 package com.dineshneupane.ncovidroid;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Objects;
 
 public class InteractiveMapActivity extends AppCompatActivity {
 
+  @SuppressLint("SetJavaScriptEnabled")
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_interactive_map);
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     getSupportActionBar().setTitle("Interactive Map");
     WebView wv = findViewById(R.id.root_web_view);
     wv.getSettings().setJavaScriptEnabled(true);
